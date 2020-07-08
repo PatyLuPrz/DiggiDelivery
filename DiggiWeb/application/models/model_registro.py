7b176f4fd5c1e59a4a0c375eadbc3a3e4af200e1
PatyLuPrz <1717110752@utectulancingo.edu.mx>
2020-07-07 23:49:56 -0500

    Generador de UID

--- DiggiWeb/application/models/model_registro.py
+++ DiggiWeb/application/models/model_registro.py
@@ -2,6 +2,7 @@ import web
 import config
 import firebase_admin
 from firebase_admin import auth
+import uuid
 
 
 def registrarUsuarios(correo,contrasena,nombre):
@@ -10,10 +11,28 @@ def registrarUsuarios(correo,contrasena,nombre):
         return False
     except:
         user = auth.create_user(
+            uid=obtenerUID(),
             email=correo,
             email_verified=False,
             password=contrasena,
             display_name=nombre,
             disabled=False)
         return True
+
+
+def obtenerUID():
+    try:
+        UID = generarUID(str(uuid.uuid4().fields[-1])[:20])
+        return UID.get_UID()
+    except Exception as e:
+        return "Error al generar UID: " + str(e.args)
         
+class generarUID:
+    def __init__(self, UID):
+        self.__UID = UID
+
+    def get_UID(self):
+        return self.__UID
+
+    def set_UID(self, UID):
+        self.__UID = UID
