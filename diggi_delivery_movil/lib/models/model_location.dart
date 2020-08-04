cc1e800adae1791d5bf5f46d39663e067f082c26
Norberto Paloma <luis_norberto1998@hotmail.com>
2020-08-04 18:45:41 -0500

    Metodo para asignar permisos de ubicación

--- diggi_delivery_movil/lib/models/model_location.dart
+++ diggi_delivery_movil/lib/models/model_location.dart
@@ -1,11 +1,31 @@
 // import 'package:location/location.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
-import '';
+import 'package:permission_handler/permission_handler.dart';
 
 class ModelLocation {
+  Future<void> _request() async {
+    final PermissionStatus status =
+        await Permission.locationWhenInUse.request();
 
-  Future<void> _request () async{
+    print("STATUS :::::::::::::::::::::::: $status");
 
+    switch (status) {
+      case PermissionStatus.undetermined:
+        
+        break;
+      case PermissionStatus.granted:
+        
+        break;
+      case PermissionStatus.denied:
+        
+        break;
+      case PermissionStatus.restricted:
+        
+        break;
+      case PermissionStatus.permanentlyDenied:
+        
+        break;
+    }
   }
 
   // LocationData _locationData;
@@ -45,6 +65,5 @@ class ModelLocation {
   //     return LatLng(latitud, longitud);
   //   }
   // }
-
 
 }
